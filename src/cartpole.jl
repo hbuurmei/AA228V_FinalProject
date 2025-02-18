44c7ee680e38c945f31c06e6f278120e66cec941
Romeo Valentin <romeov@stanford.edu>
2025-02-17 18:31:23 -0800

    Create wrapper for Julia-Python interface
    This includes adding sensor noise and such to the observations.
    Let's us start to do fuzzing etc.
    
    Example script is provided in `src/run_cartpole.jl`. Just run from repo
    base dir.

---
+++ src/cartpole.jl
@@ -0,0 +1,58 @@
+import YAML: load_file
+# const State_t = SVector{4, Float64}
+const State_t = Vector{Float64}
+struct CartPole <: Environment
+      pyenv
+      s::State_t
+end
+
+CartPole() = let pyenv = gym.envs.classic_control.CartPoleEnv()
+    CartPole(pyenv, pyconvert(State_t, pyenv.reset()[0]))
+end
+
+function (env::CartPole)(s, a, xs=missing)
+    env.pyenv.state = np.array(s)
+    s_new = env.pyenv.step(a)[0]
+    pyconvert(State_t, s_new)
+end
+
+Ps(env::CartPole) = Product([
+    Uniform(-0.05, 0.05)
+    for _ in 1:4
+])
+
+struct AdditiveNoiseSensor <: Sensor
+    Do
+end
+
+(sensor::AdditiveNoiseSensor)(s) = sensor(s, rand(Do(sensor, s)))
+(sensor::AdditiveNoiseSensor)(s, x) = s + x
+
+Do(sensor::AdditiveNoiseSensor, s) = sensor.Do
+
+# Os(sensor::AdditiveNoiseSensor) = I
+
+@enum AgentType begin
+    expert_agent
+    imitation_agent
+end
+
+struct RLAgent <: Agent
+    pyagent
+end
+RLAgent(agent_t::AgentType) = RLAgent(Val(agent_t))
+function RLAgent(::Val{expert_agent})
+    cfg = YAML.load_file("config/train/rl_agent_cartpole.yaml")
+    pyagent = RLAgent_py(cfg)
+    pyagent.load_model("data/models/expert_policy.pt")
+    pyagent.exploration_rate = 0
+    RLAgent(pyagent)
+end
+
+(agent::RLAgent)(s, a=missing) = agent.pyagent.act(np.array(s)) |> x->pyconvert(Int, x)
+
+# const Project1MediumSystem::Type = System{ProportionalController, CartPole, AdditiveNoiseSensor}
+# const Project2MediumSystem::Type = Project1MediumSystem
+const CartPoleSystem = System{RLAgent, CartPole, AdditiveNoiseSensor}
+
+get_depth(sys::CartPoleSystem) = 100
---
+++ src/run_cartpole.jl
@@ -0,0 +1,25 @@
+@assert basename(pwd()) == "AA228V_FinalProject"
+import Pkg; Pkg.activate(".")
+ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+ENV["JULIA_PYTHONCALL_EXE"] = ".venv/bin/python"
+using PythonCall
+using Revise
+using Distributions, YAML
+
+includet("system.jl")
+includet("distributions.jl")
+includet("cartpole.jl")
+
+pyimport("sys").path.append("src")
+gym = pyimport("gym"); gym.make("CartPole-v1")
+np = pyimport("numpy")
+RLAgent_py = pyimport("reinforcement_learning"=>"RLAgent")
+
+
+sys = System(
+    RLAgent(expert_agent),
+    CartPole(),
+    AdditiveNoiseSensor(Product([Deterministic(0.0), Normal(0.0, 0.5), Deterministic(0.0), Deterministic(0.0)]))
+)
+
+@show rollout(sys; d=get_depth(sys))
