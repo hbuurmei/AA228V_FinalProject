c784c953653432b2f52f41347c1a91297f35a6ba
Romeo Valentin <romeov@stanford.edu>
2025-02-20 20:40:34 -0800

    export names.

--- src/AA228V_FinalProject.jl
+++ src/AA228V_FinalProject.jl
@@ -4,8 +4,21 @@ using Distributions, YAML
 using SignalTemporalLogic
 using IntervalSets
 
+export System, RLAgent, CartPole, AdditiveNoiseSensor
+export NominalTrajectoryDistribution, DisturbanceDistribution
+export rollout, step, isfailure
+export expert_agent, imitation_agent
+
+pyimport("sys").path.append(
+    joinpath(split(ENV["JULIA_PYTHONCALL_EXE"], "/.venv")[1], "src"))
+const gym = pyimport("gym"); gym.make("CartPole-v1")
+const np = pyimport("numpy")
+const RLAgent_py = pyimport("reinforcement_learning"=>"RLAgent")
+const ILAgent_py = pyimport("imitation_learning"=>"ILAgent")
+
 include("system.jl")
 include("distributions.jl")
 include("cartpole.jl")
 include("specification.jl")
+
 end
